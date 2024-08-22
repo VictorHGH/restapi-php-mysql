f388cd4ff012785a89086be459337fa4bbb1255e
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-08-22 12:14:25 -0600

    [Added] developer controller and controllers folder

---
+++ api/src/controllers/Developer.controller.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once __DIR__ . '/../services/Developer.service.php';
+require_once __DIR__ . '/utils/ResponseMethos.php';
+
+class DeveloperController
+{
+	private $requestMethod;
+	private $developerService;
+
+	private static $instance = null;
+
+	public static function getInstance()
+	{
+		if (self::$instance === null) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	public function __construct()
+	{
+		$this->developerService = DeveloperService::getInstance();
+	}
+
+	public function processRequest()
+	{
+		$this->requestMethod = $_SERVER['REQUEST_METHOD'];
+
+		if ($this->requestMethod === 'GET') {
+			$this->getCase();
+		} else {
+			ResponseMethods::printError(400);
+		}
+	}
+
+	// HTTP Methods cases
+
+	private function getCase()
+	{
+		$this->listDevelopers();
+	}
+
+	// Private methods
+
+	private function listDevelopers()
+	{
+		try {
+			$developers = $this->developerService->listDevelopers();
+
+			if ((gettype($developers) === 'array') && is_array($developers)) {
+				if (sizeof($developers) > 0) {
+					ResponseMethods::printJSON("SUCCESS", $developers);
+				} else {
+					ResponseMethods::printJSON("NOTFOUD");
+				}
+			} else {
+				ResponseMethods::printError(500, "An undexpected error ocurred...");
+			}
+		} catch (Exception $ex) {
+			// Add exception message to Log.
+			ResponseMethods::printError(500, "An unexpected error ocurred...");
+		}
+	}
+}
